8e97e45e59eb3ae772afd4ccbb55a747a42aabf2
areeb786 <areebrockzz786@gmail.com>
2023-09-11 22:15:18 +0530

    added api key

---
+++ app/src/main/java/com/areeb/moviesverse/ui/utils/strings/CommonStrings.kt
@@ -0,0 +1,11 @@
+package com.areeb.moviesverse.ui.utils.strings
+
+interface CommonStrings {
+
+    interface API {
+        companion object {
+            private const val API_KEY =
+                "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzNhZjJjYjc0ZmJiZmE1MDBlYzliYzhkYWE1NGZhMSIsInN1YiI6IjY0OGI0Njg0YzNjODkxMDBlYjMxNDM4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QoDxOMLfPED6DPu2ypHgvza6uiQ0MFsCqU6-wW40PQU"
+        }
+    }
+}
